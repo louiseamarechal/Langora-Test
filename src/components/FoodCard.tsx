1194cc9fce8a382f8aaf25e5203bdd9028a9f703
louiseameliemarechal <louiseamelie2@gmail.com>
2023-07-13 19:43:04 +0200

    last adjustment

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View, StyleSheet, Text, Image, Platform } from "react-native";
 
 import DisplayStars from "./DisplayStar";
